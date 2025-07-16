cb31eb1be073c21e19c1eb098d62b9b54bb338ac
Bhikkhunaute <159037621+Bhikkhunaute@users.noreply.github.com>
2025-07-16 15:29:33 +0200

    Ajout de matomo

--- src/router/index.js
+++ src/router/index.js
@@ -185,7 +185,16 @@ router.beforeEach((to, from, next) => {
   }
 
   next()
+});
+
+router.afterEach((to) => {
+  if (window._paq) {
+    window._paq.push(['setCustomUrl', window.location.href])
+    window._paq.push(['setDocumentTitle', document.title])
+    window._paq.push(['trackPageView'])
+  }
 })
+
 
 
 export default router;
