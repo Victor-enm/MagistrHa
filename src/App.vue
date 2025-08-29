ebda6a8d9c1cdb2dffb6ed2e3880b650504c40a6
Bhikkhunaute <159037621+Bhikkhunaute@users.noreply.github.com>
2025-08-29 11:33:07 +0200

    Pre release waiting room

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
-  <MobileOnly>
-  <div class="min-h-screen flex flex-col w-screen">
-    <router-view />
-    <NavBarre class="sticky bottom-0 z-50" />
-  </div>
-  </MobileOnly>
+  <UpdateView />
 </template>
 
 <script>
+import UpdateView from '@/pages/UpdateView.vue';
+
 
 </script>
 
